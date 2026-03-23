f91a159c693328910d61a694acddb8d6d620c669
Devidas-G <Dev.gaikwad222@gmail.com>
2026-03-23 13:52:04 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,21 @@ import 'dependency_injection.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  const envFile = String.fromEnvironment(
+    "ENV_FILE",
+    defaultValue: ".env/.env.dev",
+  );
+  await dotenv.load(fileName: envFile);
   ApiConfig.apiKey = dotenv.get('API_KEY');
   await di.init();
   EngineInitializer.init();
   runApp(const MyApp());
 }
+
+//! For local development
+  // flutter run --dart-define=ENV_FILE=.env/.env.dev
+  // flutter run --dart-define=ENV_FILE=.env/.env.staging
+  // flutter run --dart-define=ENV_FILE=.env/.env.prod
+
+//! For build release
+  //flutter build apk --dart-define=ENV_FILE=.env/.env.prod
